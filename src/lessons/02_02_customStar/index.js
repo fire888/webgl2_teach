72dccf9c547f1fbdf74a1c677991f70b2bcdcad9
vasilii <porokh.vs@gmail.com>
2021-06-12 22:10:36 +0300

    process

--- src/lessons/02_02_customStar/index.js
+++ src/lessons/02_02_customStar/index.js
@@ -1,14 +1,45 @@
 import vShaderSrc from './shader.vert'
 import fShaderSrc from './shader.frag'
-import {createPath } from './path'
+import { createPath } from './path'
+
+
+
+
+const color1 = [Math.random(), Math.random(), Math.random(), 1]
+const color2 = [Math.random(), Math.random(), Math.random(), 1]
+const color3 = [Math.random(), Math.random(), Math.random(), 1]
+const color4 = [Math.random() / 10, Math.random() / 10, Math.random() / 10, 1]
+const color5 = [Math.random(), Math.random(), Math.random(), 1]
+
+/*
+[0.9967725302274126, 0.18212122151325083, 0.5990163427382622, 1]
+[0.4178191261311779, 0.12190126469705276, 0.3377100395569723, 1]
+[0.9406109720203257, 0.15612787068021783, 0.6048517590022973, 1]
+ */
+
+/*
+[0.7151972315239166, 0.38500599602947494, 0.3769678720718095, 1]
+[0.9780542815259796, 0.9871210336324274, 0.22119000475116524, 1]
+[0.5792843477820926, 0.17559856182245115, 0.44975051824707446, 1]
+ */
+
+
+
 
 
 const canvas = document.createElement('canvas')
-canvas.width = 400
-canvas.height = 400
+const s = Math.min(window.innerWidth, window.innerHeight)
+canvas.width = canvas.height = s
 canvas.style.border = '2px solid #000000'
 document.body.appendChild(canvas)
 const gl = canvas.getContext('webgl2')
+
+
+
+
+
+
+console.log(color1, color2, color3, color4)
 
 
 
@@ -24,6 +55,7 @@ function createShader (gl, type, shaderSrc) {
 }
 
 
+
 function createProgram (gl, vShader, fShader) {
     const program = gl.createProgram()
     gl.attachShader(program, vShader)
@@ -37,93 +69,141 @@ function createProgram (gl, vShader, fShader) {
 }
 
 
+
 const vShader = createShader(gl, gl.VERTEX_SHADER, vShaderSrc)
 const fShader = createShader(gl, gl.FRAGMENT_SHADER, fShaderSrc)
 const program = createProgram(gl, vShader, fShader)
 
 
 
-const poses = createPath(2)
+const poses = createPath()
+const poses32_0 = new Float32Array(poses.arrGeom0)
 const poses32_1 = new Float32Array(poses.arrGeom1)
-//const poses32_2 = new Float32Array(poses.arrGeom2)
+const poses32_2 = new Float32Array(poses.arrGeom2)
+
+
+
+const bufferPoses_0 = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_0)
+gl.bufferData(gl.ARRAY_BUFFER, poses32_0, gl.STATIC_DRAW)
 
 
 const bufferPoses_1 = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
-//gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
 gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
 
 
-//const bufferPoses_2 = gl.createBuffer()
-//gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
-//gl.bufferData(gl.ARRAY_BUFFER, poses32_2, gl.DYNAMIC_DRAW)
+
+const bufferPoses_2 = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
+gl.bufferData(gl.ARRAY_BUFFER, poses32_2, gl.STATIC_DRAW)
+
+
 
 
 
 const posAttributeLocation = gl.getAttribLocation(program, 'a_position')
 const colorUniformLocation = gl.getUniformLocation(program, 'u_color')
 
+
+
 gl.useProgram(program)
 
-gl.enableVertexAttribArray(bufferPoses_1)
+
+gl.enableVertexAttribArray(posAttributeLocation)
+gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_0)
+gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+gl.uniform4f(colorUniformLocation, ...color4);
+gl.drawArrays(gl.TRIANGLES, 0, poses32_0.length)
+
+
+
+gl.enableVertexAttribArray(posAttributeLocation)
 gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
 gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
 gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
 gl.drawArrays(gl.TRIANGLES, 0, poses32_1.length)
 
-// gl.enableVertexAttribArray(bufferPoses_2)
-// gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
-// gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-// gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
-// gl.drawArrays(gl.TRIANGLES, 0, poses32_2.length)
+
+gl.enableVertexAttribArray(posAttributeLocation)
+gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
+gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+gl.drawArrays(gl.TRIANGLES, 0, poses32_2.length)
 
 
 
 
 let d = 0
-const spd = 0.08
-const animate = () => {
-    d += spd
+const spd = 0.05
+var size = 2;          // 2 components per iteration
+var type = gl.FLOAT;   // the data is 32bit floats
+var normalize = false; // don't normalize the data
+var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+var offset = 0;        // start at the beginning of the buffer
 
+
+
+
+
+const animate = () => {
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(...color1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+
+    d += spd
     const poses = createPath(d)
 
-    gl.enableVertexAttribArray(bufferPoses_1)
-    //gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
-    gl.uniform4f(colorUniformLocation, (Math.sin(d) + 1) / 2 , 0, 0, 1);
 
-    for (let i = 0; i < poses32_1.length; i++) {
+
+
+
+
+
+
+    gl.uniform4f(colorUniformLocation, ...color2)
+
+    gl.enableVertexAttribArray(posAttributeLocation)
+    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
+
+    for (let i = 0; i < poses32_1.length; i ++) {
          poses32_1[i] = poses.arrGeom1[i]
     }
-
-    //  //poses[0] = (Math.sin(d) /  2)
-    //  //gl.bufferData(gl.ARRAY_BUFFER, poses32, gl.DYNAMIC_DRAW)
-    //  //gl.bufferData(gl.ARRAY_BUFFER, poses32, gl.STREAM_DRAW)
-    //  gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
     gl.drawArrays(gl.TRIANGLES, 0, poses32_1.length)
-    //
-    // gl.enableVertexAttribArray(bufferPoses_2)
-    // gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
-    // gl.uniform4f(colorUniformLocation, 0, 1, 0, 1);
-    // for (let i = 0; i < poses32_2.length; i += 4) {
-    //     poses32_2[i] = (Math.sin(d))
-    //     poses32_2[i + 1] = 0//(Math.sin(d))
-    // }
-    // d += 0.05
-    // //poses[0] = (Math.sin(d) /  2)
-    // //gl.bufferData(gl.ARRAY_BUFFER, poses32, gl.DYNAMIC_DRAW)
-    // //gl.bufferData(gl.ARRAY_BUFFER, poses32, gl.STREAM_DRAW)
-    // gl.bufferData(gl.ARRAY_BUFFER, poses32_2, gl.STATIC_DRAW)
-    // gl.drawArrays(gl.TRIANGLES, 0, poses32_2.length)
 
-     setTimeout(() => requestAnimationFrame(animate), 0)
+
+
+
+    gl.uniform4f(colorUniformLocation, ...color3)
+
+    gl.enableVertexAttribArray(posAttributeLocation)
+    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
+
+
+    for (let i = 0; i < poses32_2.length; i ++) {
+        poses32_2[i] = poses.arrGeom2[i]
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, poses32_2, gl.STATIC_DRAW)
+    gl.drawArrays(gl.TRIANGLES, 0, poses32_2.length)
+
+
+
+
+    gl.uniform4f(colorUniformLocation, ...color5)
+
+    gl.enableVertexAttribArray(posAttributeLocation)
+    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_0)
+    for (let i = 0; i < poses32_0.length; i ++) {
+        poses32_0[i] = poses.arrGeom0[i]
+    }
+    gl.bufferData(gl.ARRAY_BUFFER, poses32_0, gl.STATIC_DRAW)
+    gl.drawArrays(gl.TRIANGLES, 0, poses32_0.length)
+
+
+     requestAnimationFrame(animate)
 }
 animate()
-
-
-
-
-//      //gl.bufferData(gl.ARRAY_BUFFER, poses32, gl.DYNAMIC_DRAW)
-//      //gl.bufferData(gl.ARRAY_BUFFER, poses32, gl.STREAM_DRAW)
-//      gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
-
-
--- src/lessons/02_02_customStar/path.js
+++ src/lessons/02_02_customStar/path.js
@@ -1,110 +1,130 @@
-//let offset = [0, 0]
-
-const
-W = 0.05,
-hW = W / 2,
-length = .8,
-segments = 200,
-lenSegment = length / segments
 
 
 
-// export function createPath () {
-//     const centralLine = createCentralLine()
-//     const points = createPoints(centralLine)
-//     const polygons = createPoligons(points)
-//     const arrToExport = prepareArrToExport(polygons)
-//     return arrToExport
-// }
 
 export function createPath (val = 0) {
-    currentW = val
+    startW = val
+
     const road = createRoad()
     const polygons = createPoligonsOfRoad(road)
     const arrToExportColor1 = prepareArrToExportRoad(polygons.arrColor1)
     const arrToExportColor2 = prepareArrToExportRoad(polygons.arrColor2)
+    const arrToExportTopBevelColor3 = prepareArrToExportRoad(polygons.arrTopBevelColor)
     return {
         arrGeom1: arrToExportColor1,
         arrGeom2: arrToExportColor2,
+        arrGeom0: arrToExportTopBevelColor3,
     }
 }
 
 
 
-function createCentralLine() {
-    const arr = []
-    const start = 0
-    for (let i = 0; i < segments; ++i) {
-        arr.push([start + (i * lenSegment), 0])
-    }
-    return arr
-}
-
-
-const circles = 4
-const len = 2 * Math.PI * circles
-const stepLen = len / segments
-const stepRadius = 1.5 / segments
-const spdW = 0.7
-
-let currentW = 0
-let currentLen = 0
-let currentR = 0
-
+let startW = 0
+let start = 0
 
 function createRoad() {
+    const segments = 360
+    const circles = 4
+    const len = 2 * Math.PI * circles
+    const stepLen = len / segments
+    const stepRadius = 2 / segments
+    const spdW = 0.7
+
+
+    let currentLen = 0
+    let currentR = 0
+
+
     const arrTop = []
+    const arrTopBevel = []
     const arrBottom = []
+    const arrBottomBevel = []
+
+    let currentW = startW
+    start = startW
+
+    const bevelOffset = [-.05, -.1]
 
     for (let i = 0; i < segments; ++i) {
         currentR += stepRadius
-        currentLen += stepLen
-        currentW += spdW
-        const w = (Math.sin(currentW) + 2) / 20
 
-        arrTop.push([Math.sin(currentLen) * (currentR + w), Math.cos(currentLen) * (currentR + w)])
-        arrBottom.push([Math.sin(currentLen) * (currentR - w), Math.cos(currentLen) * (currentR - w)])
+        currentLen += stepLen
+        const d = start / 10 + currentLen
+        currentW -= spdW
+
+        const capStart = Math.min(i / 100, 1)
+        const w = ((Math.sin(currentW) + 2) / 15) * capStart
+
+        const topPoint = [Math.sin(d) * (currentR + w), Math.cos(d) * (currentR + w)]
+        arrTop.push([topPoint[0], topPoint[1]])
+        arrTopBevel.push([topPoint[0] + bevelOffset[0], topPoint[1] + bevelOffset[1]])
+
+        const bottomPoint = [Math.sin(d) * (currentR - w), Math.cos(d) * (currentR - w)]
+        arrBottom.push([bottomPoint[0], bottomPoint[1]])
     }
-    return { arrTop, arrBottom }
+    return { arrTop, arrBottom, arrTopBevel, arrBottomBevel }
 }
 
+
+
 function createPoligonsOfRoad(data) {
-    const { arrTop, arrBottom } = data
+    const { arrTop, arrBottom,  arrTopBevel, arrBottomBevel } = data
+    const arrTopBevelColor = []
+
     const arrColor1 = []
     const arrColor2 = []
     for (let i = 0; i < arrTop.length; i += 2) {
         if (!arrTop[i + 2]) continue;
 
         const p1 = [
-            [ arrTop[i][0],       arrTop[i][1]      ],
-            [ arrBottom[i][0],    arrBottom[i][1]   ],
-            [ arrTop[i + 1][0],   arrTop[i + 1][1]  ],
+            [ arrTop[i][0],         arrTop[i][1]       ],
+            [ arrBottom[i][0],      arrBottom[i][1]    ],
+            [ arrTop[i + 1][0],     arrTop[i + 1][1]   ],
         ]
 
         const p2 = [
-            [ arrTop[i + 1][0], arrTop[i + 1][1]       ],
-            [ arrBottom[i][0], arrBottom[i][1]         ],
-            [ arrBottom[i + 1][0], arrBottom[i + 1][1] ],
+            [ arrTop[i + 1][0],     arrTop[i + 1][1]    ],
+            [ arrBottom[i][0],      arrBottom[i][1]     ],
+            [ arrBottom[i + 1][0],  arrBottom[i + 1][1] ],
         ]
 
         arrColor1.push(p1, p2)
 
+
+
         const p3 = [
-            [ arrTop[i + 1][0],       arrTop[i + 1][1]      ],
-            [ arrBottom[i + 1][0],    arrBottom[i + 1][1]   ],
-            [ arrTop[i + 2][0],   arrTop[i + 2][1]  ],
+            [ arrTop[i + 1][0],     arrTop[i + 1][1]      ],
+            [ arrBottom[i + 1][0],  arrBottom[i + 1][1]   ],
+            [ arrTop[i + 2][0],     arrTop[i + 2][1]      ],
         ]
 
         const p4 = [
-            [ arrTop[i + 2][0], arrTop[i + 2][1]   ],
-            [ arrBottom[i + 1][0], arrBottom[i + 1][1]         ],
-            [ arrBottom[i + 2][0], arrBottom[i + 2][1] ],
+            [ arrTop[i + 2][0],     arrTop[i + 2][1]      ],
+            [ arrBottom[i + 1][0],  arrBottom[i + 1][1]   ],
+            [ arrBottom[i + 2][0],  arrBottom[i + 2][1]   ],
         ]
 
         arrColor2.push(p3, p4)
+
+
+        const p5 = [
+            [  arrTop[i][0],   arrTop[i][1]   ],
+            [  arrTopBevel[i][0], arrTopBevel[i][1]  ],
+            [  arrTop[i + 1][0], arrTop[i + 1][1]  ],
+        ]
+
+        const p6 = [
+            [  arrTop[i+1][0],   arrTop[i+1][1]   ],
+            [  arrTopBevel[i][0], arrTopBevel[i][1]  ],
+            [  arrTopBevel[i + 1][0], arrTopBevel[i + 1][1]  ],
+        ]
+
+        arrTopBevelColor.push(p5, p6)
+
     }
-    return { arrColor1, arrColor2 }
+    return { arrColor1, arrColor2, arrTopBevelColor }
 }
+
 
 
 function prepareArrToExportRoad(data) {
@@ -113,65 +133,6 @@ function prepareArrToExportRoad(data) {
         for (let j = 0; j < data[i].length; ++j) {
             arr.push(data[i][j][0])
             arr.push(data[i][j][1])
-        }
-    }
-    return arr
-
-}
-
-
-
-
-function createPoints(line) {
-    const arr = []
-    for (let i = 0; i < line.length; ++i) {
-        arr.push(
-            [line[i][0], line[i][1] - (width / 2)],
-            [line[i][0], line[i][1] + (width / 2)],
-        )
-    }
-    return arr
-}
-
-function createPoligons(points) {
-    const arr = []
-    for (let i = 0; i < points.length; i += 4) {
-
-        const square_1 = [
-            points[i][0], points[i][1],
-            points[i + 1][0], points[i + 1][1],
-            points[i + 2][0], points[i + 2][1],
-
-            points[i + 2][0], points[i + 2][1],
-            points[i + 1][0], points[i + 1][1],
-            points[i + 3][0], points[i + 3][1],
-        ]
-
-        arr.push(square_1)
-
-        if (points[i + 4]) {
-            const square_2 = [
-                points[i + 2][0], points[i + 2][1],
-                points[i + 3][0], points[i + 3][1],
-                points[i + 4][0], points[i + 4][1],
-
-                points[i + 4][0], points[i + 4][1],
-                points[i + 3][0], points[i + 3][1],
-                points[i + 5][0], points[i + 5][1],
-            ]
-
-           // arr.push(square_2)
-        }
-    }
-
-    return arr
-}
-
-function prepareArrToExport(poligons) {
-    const arr = []
-    for (let i = 0; i < poligons.length; ++i) {
-        for (let j = 0; j < poligons[i].length; ++j) {
-            arr.push(poligons[i][j])
         }
     }
     return arr
