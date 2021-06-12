0dfa41b9852f40b2027d86ad927fd9bf59804502
vasilii <porokh.vs@gmail.com>
2021-06-13 02:35:53 +0300

    process

--- src/lessons/02_02_customStar/index.js
+++ src/lessons/02_02_customStar/index.js
@@ -1,277 +1,154 @@
 import vShaderSrc from './shader.vert'
 import fShaderSrc from './shader.frag'
 import { createPath } from './path'
+import { createColorUpdater } from './color'
 
 
 
-
-const color1 = [Math.random(), Math.random(), Math.random(), 1]
-const color2 = [Math.random(), Math.random(), Math.random(), 1]
-const color3 = [Math.random(), Math.random(), Math.random(), 1]
-const color4 = [Math.random() / 10, Math.random() / 10, Math.random() / 10, 1]
-const color5 = [Math.random(), Math.random(), Math.random(), 1]
-
-
-const red = [1, 0, 0, 1]
-const redLight = [1, .3, .3, 1]
-const green = [0, 1, 0, 1]
-const greenLight = [0.3, 1, .3, 1]
-
-/*
-[0.9967725302274126, 0.18212122151325083, 0.5990163427382622, 1]
-[0.4178191261311779, 0.12190126469705276, 0.3377100395569723, 1]
-[0.9406109720203257, 0.15612787068021783, 0.6048517590022973, 1]
- */
-
-/*
-[0.7151972315239166, 0.38500599602947494, 0.3769678720718095, 1]
-[0.9780542815259796, 0.9871210336324274, 0.22119000475116524, 1]
-[0.5792843477820926, 0.17559856182245115, 0.44975051824707446, 1]
- */
-
-
-
-
-
-const canvas = document.createElement('canvas')
-const s = Math.min(window.innerWidth, window.innerHeight)
-canvas.width = canvas.height = s
-canvas.style.border = '2px solid #000000'
-document.body.appendChild(canvas)
-const gl = canvas.getContext('webgl2')
-
-
-
-
-
-
-console.log(color1, color2, color3, color4)
-
-
-
-function createShader (gl, type, shaderSrc) {
-    const shader = gl.createShader(type)
-    gl.shaderSource(shader, shaderSrc)
-    gl.compileShader(shader)
-    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
-    if (success) {
-        return shader
+const glUtils = () => {
+    function createGl () {
+        document.body.style.textAlign = 'center'
+        document.body.style.overflow = 'hidden'
+        const canvas = document.createElement('canvas')
+        const s = Math.min(window.innerWidth, window.innerHeight)
+        canvas.width = canvas.height = s
+        canvas.style.border = '2px solid #000000'
+        canvas.style.boxSizing = 'border-box'
+        document.body.appendChild(canvas)
+        return canvas.getContext('webgl2')
     }
-    console.log('compile shader error')
+
+
+
+    function createShader (gl, type, shaderSrc) {
+        const shader = gl.createShader(type)
+        gl.shaderSource(shader, shaderSrc)
+        gl.compileShader(shader)
+        const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS)
+        if (success) {
+            return shader
+        }
+        console.log('compile shader error')
+    }
+
+    function createProgram (gl, vShader, fShader) {
+        const program = gl.createProgram()
+        gl.attachShader(program, vShader)
+        gl.attachShader(program, fShader)
+        gl.linkProgram(program)
+        const success = gl.getProgramParameter(program, gl.LINK_STATUS)
+        if (success) {
+            return program
+        }
+        console.log('program compile error')
+    }
+
+
+    const gl = createGl()
+
+    let posAttributeLocation, colorUniformLocation
+
+    const size = 2          // 2 components per iteration
+    const type = gl.FLOAT   // the data is 32bit floats
+    const normalize = false // don't normalize the data
+    const stride = 0        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    const offset = 0        // start at the beginning of the buffer
+
+
+
+    return {
+        createBuffer (arr32) {
+            const buffer = gl.createBuffer()
+            gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+            gl.bufferData(gl.ARRAY_BUFFER, arr32, gl.STATIC_DRAW)
+            return buffer
+        },
+        prepareProgram () {
+            const vShader = createShader(gl, gl.VERTEX_SHADER, vShaderSrc)
+            const fShader = createShader(gl, gl.FRAGMENT_SHADER, fShaderSrc)
+            const program = createProgram(gl, vShader, fShader)
+
+            posAttributeLocation = gl.getAttribLocation(program, 'a_position')
+            colorUniformLocation = gl.getUniformLocation(program, 'u_color')
+
+            gl.useProgram(program)
+        },
+
+        clearCanvas(color) {
+            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+            gl.clearColor(...color)
+            gl.clear(gl.COLOR_BUFFER_BIT)
+        },
+
+        drawLayer (layer, newPoses = layer.poses, color) {
+            gl.uniform4f(colorUniformLocation, ...color)
+
+            gl.enableVertexAttribArray(posAttributeLocation)
+            gl.bindBuffer(gl.ARRAY_BUFFER, layer.buffer)
+            for (let i = 0; i < layer.poses.length; i ++) {
+                layer.poses32[i] = newPoses[i]
+            }
+            gl.bufferData(gl.ARRAY_BUFFER, layer.poses32, gl.STATIC_DRAW)
+
+            gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
+            gl.drawArrays(gl.TRIANGLES, 0, layer.poses32.length)
+        }
+    }
 }
 
 
 
-function createProgram (gl, vShader, fShader) {
-    const program = gl.createProgram()
-    gl.attachShader(program, vShader)
-    gl.attachShader(program, fShader)
-    gl.linkProgram(program)
-    const success = gl.getProgramParameter(program, gl.LINK_STATUS)
-    if (success) {
-        return program
+const createLayersManager = (glU, poses) => {
+    const layers = [
+        { key: 'geomBevel01_00' },
+        { key: 'geomBevel01_01' },
+        { key: 'geomBevel02_00' },
+        { key: 'geomBevel02_01' },
+        { key: 'geomTop01' },
+        { key: 'geomTop02' },
+    ]
+
+    for (let i = 0; i < layers.length; ++i) {
+        const { key } = layers[i]
+
+        if (!poses[key]) continue;
+
+        layers[i].poses = poses[key]
+        layers[i].poses32 = new Float32Array(poses[key])
+
+        layers[i].buffer = glU.createBuffer(layers[i].poses32)
     }
-    console.log('program compile error')
+
+    return {
+        update (poses, color) {
+            for (let i = 0; i < layers.length; ++i) {
+                const { key } = layers[i]
+                if (!poses[key]) continue;
+
+                glU.drawLayer(layers[i], poses[key], color[key])
+            }
+        }
+    }
 }
 
 
 
-const vShader = createShader(gl, gl.VERTEX_SHADER, vShaderSrc)
-const fShader = createShader(gl, gl.FRAGMENT_SHADER, fShaderSrc)
-const program = createProgram(gl, vShader, fShader)
 
+const colorUpdater = createColorUpdater()
+const glU = glUtils()
+const layerManager = createLayersManager(glU, createPath())
+glU.prepareProgram()
 
-
-const poses = createPath()
-const poses32_0 = new Float32Array(poses.arrGeom0)
-const poses32_01 = new Float32Array(poses.arrGeom01)
-const poses32_1 = new Float32Array(poses.arrGeom1)
-const poses32_2 = new Float32Array(poses.arrGeom2)
-
-
-
-
-const bufferPoses_0 = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_0)
-gl.bufferData(gl.ARRAY_BUFFER, poses32_0, gl.STATIC_DRAW)
-
-const bufferPoses_01 = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_01)
-gl.bufferData(gl.ARRAY_BUFFER, poses32_01, gl.STATIC_DRAW)
-
-
-const bufferPoses_1 = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
-gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
-
-
-
-const bufferPoses_2 = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
-gl.bufferData(gl.ARRAY_BUFFER, poses32_2, gl.STATIC_DRAW)
-
-
-
-
-
-const posAttributeLocation = gl.getAttribLocation(program, 'a_position')
-const colorUniformLocation = gl.getUniformLocation(program, 'u_color')
-
-
-
-gl.useProgram(program)
-
-
-gl.enableVertexAttribArray(posAttributeLocation)
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_0)
-gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-gl.uniform4f(colorUniformLocation, ...color5);
-gl.drawArrays(gl.TRIANGLES, 0, poses32_0.length)
-
-
-gl.enableVertexAttribArray(posAttributeLocation)
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_01)
-gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-gl.uniform4f(colorUniformLocation, ...color5);
-gl.drawArrays(gl.TRIANGLES, 0, poses32_01.length)
-
-
-
-gl.enableVertexAttribArray(posAttributeLocation)
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
-gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
-gl.drawArrays(gl.TRIANGLES, 0, poses32_1.length)
-
-
-gl.enableVertexAttribArray(posAttributeLocation)
-gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
-gl.vertexAttribPointer(posAttributeLocation, 2, gl.FLOAT, false, 0, 0)
-gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
-gl.drawArrays(gl.TRIANGLES, 0, poses32_2.length)
-
-
-
-
-let d = 0
+let dist = 0
 const spd = 0.05
-var size = 2;          // 2 components per iteration
-var type = gl.FLOAT;   // the data is 32bit floats
-var normalize = false; // don't normalize the data
-var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-var offset = 0;        // start at the beginning of the buffer
-
-
-
-
-
 const animate = () => {
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    gl.clearColor(...color1)
-    gl.clear(gl.COLOR_BUFFER_BIT)
+    const color = colorUpdater.update()
+    glU.clearCanvas(color.back)
 
-    d += spd
-    const poses = createPath(d)
+    dist += spd
+    const poses = createPath(dist)
+    layerManager.update(poses, color)
 
-
-
-
-
-    gl.uniform4f(colorUniformLocation, ...red)
-
-    gl.enableVertexAttribArray(posAttributeLocation)
-    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_0)
-    for (let i = 0; i < poses32_0.length; i ++) {
-        poses32_0[i] = poses.arrGeom0[i]
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, poses32_0, gl.STATIC_DRAW)
-    gl.drawArrays(gl.TRIANGLES, 0, poses32_0.length)
-
-
-
-
-
-
-    gl.uniform4f(colorUniformLocation, ...redLight)
-
-    gl.enableVertexAttribArray(posAttributeLocation)
-    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_2)
-
-
-    for (let i = 0; i < poses32_2.length; i ++) {
-        poses32_2[i] = poses.arrGeom2[i]
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, poses32_2, gl.STATIC_DRAW)
-    gl.drawArrays(gl.TRIANGLES, 0, poses32_2.length)
-
-
-
-
-
-
-    gl.uniform4f(colorUniformLocation, ...green)
-
-    gl.enableVertexAttribArray(posAttributeLocation)
-    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_1)
-
-    for (let i = 0; i < poses32_1.length; i ++) {
-        poses32_1[i] = poses.arrGeom1[i]
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, poses32_1, gl.STATIC_DRAW)
-    gl.drawArrays(gl.TRIANGLES, 0, poses32_1.length)
-
-
-
-
-    gl.uniform4f(colorUniformLocation, ...greenLight)
-
-    gl.enableVertexAttribArray(posAttributeLocation)
-    gl.vertexAttribPointer(posAttributeLocation, size, type, normalize, stride, offset)
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferPoses_01)
-    for (let i = 0; i < poses32_01.length; i ++) {
-        poses32_01[i] = poses.arrGeom01[i]
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, poses32_01, gl.STATIC_DRAW)
-    gl.drawArrays(gl.TRIANGLES, 0, poses32_01.length)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //
-    //
-    //
-    //
-
-
-
-
-
-
-
-
-
-
-
-     requestAnimationFrame(animate)
+    requestAnimationFrame(animate)
 }
 animate()
--- src/lessons/02_02_customStar/path.js
+++ src/lessons/02_02_customStar/path.js
@@ -1,22 +1,15 @@
 
-
-
-
 export function createPath (val = 0) {
     startW = val
 
     const road = createRoad()
     const polygons = createPoligonsOfRoad(road)
-    const arrToExportColor1 = prepareArrToExportRoad(polygons.arrColor1)
-    const arrToExportColor2 = prepareArrToExportRoad(polygons.arrColor2)
-    const arrToExportTopBevelColor3 = prepareArrToExportRoad(polygons.arrTopBevelColor)
-    const arrToExportTopBevelColor4 = prepareArrToExportRoad(polygons.arrBottomBevelColor)
-    return {
-        arrGeom1: arrToExportColor1,
-        arrGeom2: arrToExportColor2,
-        arrGeom0: arrToExportTopBevelColor3,
-        arrGeom01: arrToExportTopBevelColor4,
+    const prepared = {}
+    for (let key in polygons) {
+        prepared[key] = prepareArrToExportRoad(polygons[key])
     }
+
+    return { ...prepared }
 }
 
 
@@ -32,10 +25,8 @@ function createRoad() {
     const stepRadius = 2 / segments
     const spdW = 0.7
 
-
     let currentLen = 0
     let currentR = 0
-
 
     const arrTop = []
     const arrTopBevel = []
@@ -72,43 +63,59 @@ function createRoad() {
 
 function createPoligonsOfRoad(data) {
     const { arrTop, arrBottom,  arrTopBevel, arrBottomBevel } = data
-    const arrTopBevelColor = []
-    const arrBottomBevelColor = []
 
-    const arrColor1 = []
-    const arrColor2 = []
+
+    const geomTop01 = []
+    const geomBevel01_00 = []
+    const geomBevel01_01 = []
+    const geomTop02 = []
+    const geomBevel02_00 = []
+    const geomBevel02_01 = []
+
     for (let i = 0; i < arrTop.length; i += 2) {
         if (!arrTop[i + 2]) continue;
 
         const [ p1, p2 ] = createPolygon(arrTop[i], arrBottom[i], arrTop[i + 1], arrBottom[i + 1])
-        arrColor1.push(p1, p2)
+        geomTop01.push(p1, p2)
 
         const [ p3, p4 ] = createPolygon(arrTop[i + 1], arrBottom[i + 1], arrTop[i + 2], arrBottom[i + 2])
-        arrColor2.push(p3, p4)
-
+        geomTop02.push(p3, p4)
 
         const [ p5, p6 ] = createPolygon(arrTop[i], arrTopBevel[i], arrTop[i + 1], arrTopBevel[i + 1])
-        arrTopBevelColor.push(p5, p6)
+        geomBevel01_00.push(p5, p6)
 
+        const [ p5_1, p6_1 ] = createPolygon(arrTop[i + 1], arrTopBevel[i + 1], arrTop[i + 2], arrTopBevel[i + 2])
+        geomBevel01_01.push(p5_1, p6_1)
 
         const [ p7, p8 ] = createPolygon(arrBottom[i], arrBottomBevel[i], arrBottom[i + 1], arrBottomBevel[i + 1])
-        arrBottomBevelColor.push(p7, p8)
+        geomBevel02_00.push(p7, p8)
 
+        const [ p7_1, p8_1 ] = createPolygon(arrBottom[i + 1], arrBottomBevel[i + 1], arrBottom[i + 2], arrBottomBevel[i + 2])
+        geomBevel02_01.push(p7_1, p8_1)
     }
-    return { arrColor1, arrColor2, arrTopBevelColor, arrBottomBevelColor }
+    return {
+        geomTop01,
+        geomBevel01_00,
+        geomBevel01_01,
+        geomTop02,
+        geomBevel02_00,
+        geomBevel02_01,
+    }
 }
 
-function createPolygon(coord1, coord2, coord3, coord4) {
+
+
+function createPolygon(c1, c2, c3, c4) {
     return [
         [
-            [ coord1[0], coord1[1] ],
-            [ coord2[0], coord2[1] ],
-            [ coord3[0], coord3[1] ],
+            [ c1[0], c1[1] ],
+            [ c2[0], c2[1] ],
+            [ c3[0], c3[1] ],
         ],
         [
-            [ coord3[0], coord3[1] ],
-            [ coord2[0], coord2[1] ],
-            [ coord4[0], coord4[1] ],
+            [ c3[0], c3[1] ],
+            [ c2[0], c2[1] ],
+            [ c4[0], c4[1] ],
         ]
     ]
 }
