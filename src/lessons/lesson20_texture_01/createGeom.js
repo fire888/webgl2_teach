e0510c4ddb173cdd3320b6d1c1f5b2a5d73621b9
vasilii <porokh.vs@gmail.com>
2021-11-14 21:27:30 +0300

    process

--- src/lessons/lesson20_texture_01/createGeom.js
+++ src/lessons/lesson20_texture_01/createGeom.js
@@ -1,79 +1,196 @@
 
 
+const createGeometry = () => {
+    const points = []
+    const uv = []
+    const normals = []
+
+    return {
+        add4Points: (data) => {
+            if (data.length !== 4) {
+                console.log('add4Points data mistake', data)
+            }
+
+            points.push(
+                data[0][0], data[0][1], data[0][2],
+                data[1][0], data[1][1], data[1][2],
+                data[2][0], data[2][1], data[2][2],
+
+                data[1][0], data[1][1], data[1][2],
+                data[3][0], data[3][1], data[3][2],
+                data[2][0], data[2][1], data[2][2],
+            )
+            uv.push(
+                0, 0,
+                0, 1,
+                1, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+            )
+            normals.push(
+                0, 0, 1,
+                0, 0, 1,
+                0, 0, 1,
+
+                0, 0, 1,
+                0, 0, 1,
+                0, 0, 1,
+            )
+        },
+        getCompletedGeometry: () => {
+            return {
+                points: new Float32Array(points),
+                uv: new Float32Array(uv),
+                normals: new Float32Array(normals),
+            }
+        }
+    }
+}
+
+
+const createrGeom = createGeometry()
+
 const { PI, sin, cos } = Math
 const PI2 = PI * 2
-const arrPoses = []
-const center = [0, 0, 0]
+const R = 2.5
+
+
+//help ring X
+const t = 40
+for (let i = 0; i < t; i ++) {
+    if (i < t - 1) {
+        createrGeom.add4Points([
+            [ R * sin(i / t * PI2),        -.1,     R * cos(i / t * PI2), ],
+            [ R * sin((i + 1) / t * PI2),  -.1,     R * cos((i + 1) / t * PI2) ],
+            [ R * sin(i / t * PI2),        .1,      R * cos(i / t * PI2), ],
+            [ R * sin((i + 1) / t * PI2),  .1,      R * cos((i + 1) / t * PI2) ],
+        ])
+    }
+    else {
+        createrGeom.add4Points([
+            [ R * sin(i / t * PI2),        -.1,     R * cos(i / t * PI2), ],
+            [ R * sin(0),  -.1,     R * cos(0) ],
+            [ R * sin(i / t * PI2),        .1,      R * cos(i / t * PI2), ],
+            [ R * sin(0),  .1,      R * cos(0) ],
+        ])
+    }
+}
+// help ring Y
+for (let i = 0; i < t; i ++) {
+    if (i < t - 1) {
+        createrGeom.add4Points([
+            [  .1, R * sin(i / t * PI2),             R * cos(i / t * PI2), ],
+            [  .1, R * sin((i + 1) / t * PI2),      R * cos((i + 1) / t * PI2) ],
+            [ -.1, R * sin(i / t * PI2),              R * cos(i / t * PI2), ],
+            [ -.1,  R * sin((i + 1) / t * PI2),       R * cos((i + 1) / t * PI2) ],
+        ])
+    }
+    else {
+        createrGeom.add4Points([
+            [ .1, R * sin(i / t * PI2),           R * cos(i / t * PI2), ],
+            [ .1, R * sin(0),       R * cos(0) ],
+            [ -.1,R * sin(i / t * PI2),              R * cos(i / t * PI2), ],
+            [ -.1,R * sin(0),        R * cos(0) ],
+        ])
+    }
+}
+
+
 const nums = 20
-const offset1 = 1
-const offset2 = .3
-const offset3 = .5
+const offset1 = .5
+const offset2 = 2.
+const quality = 3
 
-for (let i = 0; i < nums; i++) {
-    arrPoses.push(
-        sin(i / nums * PI2) * offset1,             cos(i / nums * PI2) * offset1,              0,
-        sin(i / nums * PI2) * (offset1 + offset2), cos(i / nums * PI2) * (offset1 + offset2),  0,
-    )
+for (let j = 0; j < quality - 1; j++) {
+    const fStart = offset1 + (j * (offset2 / quality))
+    const fEnd = offset1 + ((j + 1) * (offset2 / quality))
 
-    if (i % 4 === 0) {
-        arrPoses.push(
-            sin(i / nums * PI2) * (offset1 + offset2), cos(i / nums * PI2) * (offset1 + offset2),  0,
-            sin(i / nums * PI2) * (offset1 + offset2 + offset3), cos(i / nums * PI2) * (offset1 + offset2 + offset3),  0,
+    for (let i = 0; i < nums; ++i) {
+        if (i !== nums - 1) {
+            const inner = []
+            const outer = []
+            {
+                const d = (i / nums) * PI2
+                const xInner = sin(d) * fStart
+                const yInner = cos(d) * fStart
+                const xOuter = sin(d) * fEnd
+                const yOuter = cos(d) * fEnd
 
-            sin((i + 1) / nums * PI2) * (offset1 + offset2), cos((i + 1) / nums * PI2) * (offset1 + offset2),  0,
-            sin((i + 1) / nums * PI2) * (offset1 + offset2 + offset3), cos((i + 1) / nums * PI2) * (offset1 + offset2 + offset3),  0,
-        )
+                const zInner = R * (cos(xInner / R) + cos(yInner / R)) / 2
+                const zOuter = R * (cos(xOuter / R) + cos(yOuter / R)) / 2
+
+                inner.push(xInner, yInner, zInner)
+                outer.push(xOuter, yOuter, zOuter)
+            }
+
+            const innerNext = []
+            const outerNext = []
+            {
+                const d = (i + 1 / nums) * PI2
+                const xInner = sin(d) * fStart
+                const yInner = cos(d) * fStart
+                const xOuter = sin(d) * fEnd
+                const yOuter = cos(d) * fEnd
+
+                const zInner = R * (cos(xInner / R) + cos(yInner / R)) / 2
+                const zOuter = R * (cos(xOuter / R) + cos(yOuter / R)) / 2
+
+                innerNext.push(xInner, yInner, zInner)
+                outerNext.push(xOuter, yOuter, zOuter)
+            }
+
+
+
+            createrGeom.add4Points([
+                [...inner],
+                [...outer],
+                [...innerNext],
+                [...outerNext],
+            ])
+
+        }
+
+     // arrPoses.push(
+     //     xInner, yInner,  zInner,
+     //     xOuter, yOuter,  zOuter,
+     // )
+
+
+
+
+
+
     }
 }
 
-const arrPosesGeom = []
-const arrPosesNormals = []
-const arrTextureCoords = []
-for (let i = 0; i < arrPoses.length; i += 6) {
-    if (arrPoses[i + 6]) {
-        arrPosesGeom.push(
-            arrPoses[i + 0],    arrPoses[i + 1],    arrPoses[i + 2],
-            arrPoses[i + 6],    arrPoses[i + 7],    arrPoses[i + 8],
-            arrPoses[i + 3],    arrPoses[i + 4],    arrPoses[i + 5],
 
-            arrPoses[i + 6],    arrPoses[i + 7],    arrPoses[i + 8],
-            arrPoses[i + 9],    arrPoses[i + 10],   arrPoses[i + 11],
-            arrPoses[i + 3],    arrPoses[i + 4],    arrPoses[i + 5],
-        )
-    } else {
-        const n = arrPoses.length - 1
+//
+// for (let i = 0; i < nums; i++) {
+//     const d = (i / nums) * PI2
+//     const  xInner = sin(d) * offset1
+//     const  yInner = cos(d) * offset1
+//     const  xOuter = sin(d) * (offset1 + offset2)
+//     const  yOuter = cos(d) * (offset1 + offset2)
+//
+//     const zInner = R * (cos(xInner / R) + cos(yInner / R)) / 2
+//     const zOuter = R * (cos(xOuter / R) + cos(yOuter / R)) / 2
+//
+//
+//     arrPoses.push(
+//         xInner, yInner,  zInner,
+//         xOuter, yOuter,  zOuter,
+//     )
+// }
 
-        console.log(arrPoses[n])
-        arrPosesGeom.push(
-            arrPoses[i + 0],    arrPoses[i + 1],    arrPoses[i + 2],
-            arrPoses[0],        arrPoses[1],        arrPoses[2],
-            arrPoses[i + 3],    arrPoses[i + 4],    arrPoses[i + 5],
 
-            arrPoses[0],        arrPoses[1],        arrPoses[2],
-            arrPoses[3],        arrPoses[4],        arrPoses[5],
-            arrPoses[i + 3],    arrPoses[i + 4],    arrPoses[i + 5],
-        )
-    }
 
-    arrPosesNormals.push(
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
 
-        0, 0, 1,
-        0, 0, 1,
-        0, 0, 1,
-    )
 
-    arrTextureCoords.push(
-        0, 0,
-        0, 1,
-        1, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-    )
-}
+
+
+
+
 
 
 const createTexture = () => {
@@ -86,12 +203,17 @@ const createTexture = () => {
     return arr
 }
 
-export const createPoints = () => ({
-    points: new Float32Array(arrPosesGeom),
-    texCoords: new Float32Array(arrTextureCoords),
-    normals: new Float32Array(arrPosesNormals),
-    texture: new Uint8Array(createTexture())
-})
+
+
+export const createPoints = () => {
+    const { points, uv, normals } = createrGeom.getCompletedGeometry()
+    return {
+        points,
+        texCoords: uv,
+        normals,
+        texture: new Uint8Array(createTexture())
+    }
+}
 
 
 
